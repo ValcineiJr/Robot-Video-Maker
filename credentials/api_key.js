cc56f4a17e8df9db5e031f2c05df08e843c7a8d0
Valcinei Jr <hakjog@gmail.com>
2020-07-14 13:32:01 -0300

    feat: add google images search

--- credentials/api_key.js
+++ credentials/api_key.js
@@ -3,4 +3,6 @@ module.exports = {
   WatsonKEY: "GQEWPmEzrZx-NNfKYUD9jhSkS-Ttb5zogGdVNr81ZuDv",
   WatsonURL:
     "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/597b8bd3-ab4e-483f-a3c3-b8d5df273c72",
+  GoogleAPIKEY: "AIzaSyClzeJz9_pzdze59_rR64_mJpUend0ggNM",
+  SearchID: "016166041225572836607:ux6oenphspk",
 };
